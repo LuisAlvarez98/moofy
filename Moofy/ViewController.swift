287bb75d890065abfef5143d2f2f4afc50c504ec
LuisAlvarez98 <luisfalvarezsanchez@gmail.com>
2020-11-20 19:57:32 -0600

    Init project

---
+++ Moofy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moofy
+//
+//  Created by Luis Felipe Alvarez Sanchez on 20/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
